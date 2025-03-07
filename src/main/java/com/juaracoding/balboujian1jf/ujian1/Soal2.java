75cf740b140a4fcf599c03d8b4dc1366d217ca69
Balbo <achmadsesarbalbo@gmail.com>
2025-03-08 04:57:20 +0700

    Update

--- src/main/java/com/juaracoding/balboujian1jf/ujian1/Soal2.java
+++ src/main/java/com/juaracoding/balboujian1jf/ujian1/Soal2.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Soal2 {
     public static void main(String[] args) {
-        // Membuat objek Scanner untuk menerima input
         Scanner input = new Scanner(System.in);
 
         // Memasukkan nama film
